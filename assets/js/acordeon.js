cba8ecfe6180324f75863b224cb4f5c37fb416ad
IKurone <xande.cavalcante.costa@gmail.com>
2025-10-16 23:49:24 -0300

    feat: add portfolio website structure with accordion functionality and user profile data

---
+++ assets/js/acordeon.js
@@ -0,0 +1,8 @@
+const acordeonTriggers = document.querySelectorAll('.acordeon .trigger');
+
+acordeonTriggers.forEach(trigger => {
+    trigger.addEventListener('click', () => {
+        const acordeon = trigger.parentElement;
+        acordeon.classList.toggle('open');
+    })
+});
