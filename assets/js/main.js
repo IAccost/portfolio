50b97a84f3e427637831ff6bb496c37f3cee8250
IKurone <xande.cavalcante.costa@gmail.com>
2025-10-17 20:41:01 -0300

    Add new image assets for PHP, PostgreSQL, Python, React, Swift, and VSCode
    - Added php.png to represent PHP.
    - Added postgresql.png for PostgreSQL.
    - Added python.png for Python.
    - Added react.png for React.
    - Added swift.png for Swift.
    - Added vscode.png for Visual Studio Code.
    
    These images will be used to enhance the visual representation of the respective technologies in the application.

---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById('profile-photo');
+    photo.src = profileData.photo;
+    photo.alt = "Foto de " + profileData.name;
+    const name = document.getElementById('profile-name');
+    name.textContent = profileData.name;
+    const job = document.getElementById('profile-job');
+    job.textContent = profileData.job;
+    const location = document.getElementById('profile-location')
+    location.textContent = profileData.location;
+    const phone = document.getElementById('profile-phone');
+    phone.textContent = profileData.phone;
+    phone.href = "tel:" + profileData.phone;
+    const email = document.getElementById('profile-email');
+    email.textContent = profileData.email;
+    email.href = "mailto:" + profileData.email;
+}
+
+function updateHardSkills(profileData) {
+    const hardSkills = document.getElementById('profile-hard-skills');
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>
+`).join('');
+}
+
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile-soft-skills');
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('');
+}
+
+function updateLanguages(profileData) {
+    const languages = document.getElementById('profile-languages');
+    languages.innerHTML = profileData.languages.map(lang => `<li>${lang}</li>`).join('');
+}
+
+function updatePortfolioProjects(profileData) {
+    const projects = document.getElementById('profile-portfolio-projects');
+    projects.innerHTML = profileData.portfolio.map(project => `<li><h3 class="title ${project.github ? 'github' : ''}">${project.name}</h3><a href="${project.url}" target="_blank">Link do Projeto</a></li>`).join('');
+}
+
+function updateExperience(profileData) {
+    const experience = document.getElementById('profile-experience');
+    experience.innerHTML = profileData.professionalExperience.map(exp => `
+                    <li>
+                        <h3 class="title">${exp.name}</h3>
+                        <p class="period">${exp.period}</p>
+                        <p>
+                            ${exp.description}
+                        </p>
+                    </li>`).join('');
+}
+
+(async () => {
+    const profileData = await fetchProfileData();
+    updateProfileInfo(profileData);
+    updateHardSkills(profileData);
+    updateSoftSkills(profileData);
+    updateLanguages(profileData);
+    updatePortfolioProjects(profileData);
+    updateExperience(profileData);
+    console.log(profileData);
+})();
